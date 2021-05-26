cb07c4d5147062d8ac709c86bcd1e3d8c69d0171
gaurishukla60 <75981006+gaurishukla60@users.noreply.github.com>
2021-05-26 18:51:43 +0530

    Add files via upload

--- cake_main.js
+++ cake_main.js
@@ -1,21 +1,50 @@
 img="";
+status="";
+objects=[];
 function setup()
 {
     canvas = createCanvas(640 , 420);
     canvas.center();
-    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 
 }
 function preload()
 {
-  img = loadImage('Cake.jpg')
+  img = loadImage('Bike.jpg')
 }
+function modelLoaded()
+ { 
+   console.log("Model Loaded!");
+    status = true;
+     objectDetector.detect(img, gotResult);
+    
+    }
+     function gotResult(error, results)
+      { 
+        if (error)
+         {
+            console.log(error); 
+ } 
+    console.log(results); 
+     objects = results;
+}
+
 function draw()
 {
     image(img , 0 ,0,640,420);
-    fill("#FF0000");
-    text("cake", 65 ,85);
-    noFill();
-    stroke("#FF0000");
-    rect(60 , 70, 400 , 300);
+     if(status!="")
+    {
+      for(i=0; i < objects.length; i++)
+      {
+        document.getElementById("status").innerHTML="status: object detected";
+        fill("#FF0000");
+        percent=floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+        noFill();
+         stroke("#FF0000");
+         rect(objects[i].x,objects[i].y,objects[i].width + 50,objects[i].height + 50);
+         
+      }
+    }
 }
